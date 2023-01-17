ae841aadce1a2985b27e1cab0af1940f32e81df8
Allen <allen@MacBook-Air.local>
2023-01-18 01:18:34 +0200

    finalizing search

--- src/interfaces/categoryInterface.ts
+++ src/interfaces/categoryInterface.ts
@@ -1,6 +1,12 @@
 export interface categoryData {
-    _id: string;
-    title: string;
-    createdAt: string;
-    updatedAt: string;
+  _id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface dropDownCategoriesModel {
+  label: string;
+  value: string | number;
+  custom?: React.ReactNode;
 }
